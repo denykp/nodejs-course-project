b1ab70b79a10139013e8a8a759b435e7aabd4d4c
Deny <denykpamuji@gmail.com>
2023-10-18 11:30:30 +0700

    add routes and html view

--- app.js
+++ app.js
@@ -1,42 +1,21 @@
-const http = require("http");
+// import { createServer } from "http";
 
-const server = http.createServer((req, res) => {
-  const fs = require("fs");
-  if (req.url === "/") {
-    res.write(`<html>`);
-    res.write(`<h3>Hello, welcome to / pages</h3>`);
-    res.write(
-      `<form action="/create-user" method="POST"><input type="text" name="username"/><button type="submit">Submit</button></form>`
-    );
-    res.write(`</html>`);
-    res.end();
-  } else if (req.url === "/users") {
-    res.write(`<html>`);
-    res.write(`<ul>`);
-    res.write(`<li>User 1</li>`);
-    res.write(`<li>User 2</li>`);
-    res.write(`<li>User 3</li>`);
-    res.write(`</ul>`);
-    res.write(`</html>`);
-    res.end();
-  }
-  if (req.url === "/create-user" && req.method === "POST") {
-    const body = [];
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
 
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const username = parsedBody.split("=")[1];
-      console.log("testlog", username);
-      // fs.writeFileSync("username.txt", username);
-    });
+const app = express();
 
-    res.statusCode = 302;
-    res.setHeader("Location", "/");
-    res.end();
-  }
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((_, res) => {
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
-server.listen(3001);
+app.listen(3001);
